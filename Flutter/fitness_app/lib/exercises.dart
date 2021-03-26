415abb13dc6cdcbf8ed6ea173109aa716e1fea43
geoorge1d127 <george1d127@hotmail.com>
2021-03-26 14:27:26 -0600

    Removed .dart_tools

---
+++ Flutter/fitness_app/lib/exercises.dart
@@ -0,0 +1,74 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+
+class Exercises extends StatelessWidget {
+  @override
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Exercises",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 20.0,
+                ),
+                SizedBox(
+                    height: 150.0,
+                    child: GestureDetector(
+                        onTap: () {
+                          log("TEST");
+                        },
+                        child: Image.asset('assets/muscle.jpg',
+                            fit: BoxFit.contain))),
+                Text("Weight Lifting",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 50.0,
+                ),
+                SizedBox(
+                    height: 150.0,
+                    child: GestureDetector(
+                        onTap: () {
+                          log("TEST");
+                        },
+                        child: Image.asset('assets/stretch.png',
+                            fit: BoxFit.contain))),
+                Text("Stretches",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 50.0,
+                ),
+                SizedBox(
+                    height: 150.0,
+                    child: GestureDetector(
+                        onTap: () {
+                          log("TEST");
+                        },
+                        child: Image.asset('assets/cardio.jpg',
+                            fit: BoxFit.contain))),
+                Text("Cardio",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 15.0,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
