d48cf909271945f495c0731a871a3b0f022a1d81
Carpenter <lcarpen@sandia.gov>
2021-04-02 10:49:11 -0600

    not sure if this works

--- fitnessServer/src/app/app.py
+++ fitnessServer/src/app/app.py
@@ -5,18 +5,30 @@ from .views.users import user_bp
 from .views.workouts import workouts_bp
 from .views.exercises import exercises_bp
 from .views.etc import test_bp, err_bp
+from flask_mysqldb import MySQL
+from flask_login import LoginManager
+#from config import config
+db=MySQL()
 
-
+# login_manager=LoginManager()
 
 def create_app():
      # Create Flask appliaction
     app = Flask(__name__, instance_relative_config=True)
+#     app.secret_key = 'super secret string' 
+#     login_manager.init_app(app)
+#     app.config['MYSQL_HOST']=os.environ.get('HOST')
+#     app.config['MYSQL_USER']=os.environ.get('USER')
+#     app.config['MYSQL_PASSWORD']=os.environ.get('PASSWORD')
+#     app.config['MYSQL_gitDB']=os.environ.get('DATABASE')
+
+    # REgister Bps
     app.register_blueprint(user_bp)
     app.register_blueprint(exercises_bp)
     app.register_blueprint(test_bp)
     app.register_blueprint(err_bp)
     app.register_blueprint(workouts_bp)
-    print("WE ARE RUNNING", file=sys.stderr)
+    print("WE ARE RUNNING")
     
     # CORS(app)
     # Load default src/config.py
