44769ed7a9686b07405d4e4dda51cb53207250bc
Olivia Hon <ohon@sandia.gov>
2021-04-03 17:23:20 -0600

    [SQL] Adjustment to exercise logic for pulling related workout information.

--- fitnessServer/src/app/models/exercises.py
+++ fitnessServer/src/app/models/exercises.py
@@ -47,5 +47,5 @@ def db_remove_exercise(id):
     insert_or_set_data(query)
 
 def db_lookup_workouts_by_exercise(exercise_id):
-    query = f'SELECT DISTINCT w.* from ExerciseWorkoutLinks as e INNER JOIN Workouts as w on e.workoutID = w.id WHERE e.exerciseId = {exercise_id};'
+    query = f'SELECT DISTINCT w.id, w.nickname, w.ownerId from ExerciseWorkoutLinks as e INNER JOIN Workouts as w on e.workoutID = w.id WHERE e.exerciseId = {exercise_id};'
     return get_data(query)
