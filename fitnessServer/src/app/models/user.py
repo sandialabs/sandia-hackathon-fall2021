200bf515f58257494f5196aeb5a8bdc151e2c86e
Olivia Hon <ohon@sandia.gov>
2021-04-23 14:38:54 -0600

    Stubs for user demo.

--- fitnessServer/src/app/models/user.py
+++ fitnessServer/src/app/models/user.py
@@ -21,25 +21,4 @@ def insert_or_set_data(query):
     db.connection.commit()
     cursor.close()
 
-def db_enter_user(username, password, firstName, lastName, email, userStatus=0):
-    query=f'INSERT INTO Users(username,password,firstName, lastName, email, userStatus) VALUES({username},{password},{firstName},{lastName},{email}, {userStatus}));'
-    insert_or_set_data(query)
-    return
-
-def db_lookup_user(id):
-    query=f"SELECT id, username, password, email, userStatus from Users where id={id};"
-    return get_data(query)
-
-def db_lookup_user_by_name(username):
-    query=f"SELECT id, username, password, email, userStatus from Users where username={username};"
-    return get_data(query)
-
-# DO WE NEED??
-def db_update_user(id,username, password, firstName, lastName, email, userStatus):
-    query=f'UPDATE USERS SET username={username}, password={password}, firstName={firstName}, lastName={lastName}, email={email}, userStatus={userStatus} WHERE id={id}'
-    insert_or_set_data(query)
-
-def db_remove_user(id):
-    query=f'DELETE FROM Users WHERE id={id};'
-    insert_or_set_data(query)
-    
+## data access to the database needs to be filled in here!
